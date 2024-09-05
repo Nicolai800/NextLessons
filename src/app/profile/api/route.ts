2061b76bced69c63363fc5fc99cb5947f34d5390
Nicolai.Parshutin <happy454420@gmail.com>
2024-09-05 13:35:59 +0200

    42 - Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,15 +1,24 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeadres = new Headers(request.headers);
   const headerList = headers();
+
+const theme = request.cookies.get("theme");
+cookies().set("resultsPerPage", "20")
+
   console.log(requestHeadres.get("Authorization"));
   console.log(headerList.get("Authorization"));
+
+  console.log(theme);
+  console.log(cookies().get("resultsPerPage"));
+  
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
     },
   });
 }
