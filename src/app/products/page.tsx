eef79d58ca6356147d9248400f9716d4047a9a36
Nicolai.Parshutin <happy454420@gmail.com>
2024-09-12 16:18:29 +0200

    66 - Caching Data

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,27 +1,53 @@
-import Link from "next/link";
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+};
 
-export default function ProductList() {
-  const productId = 100;
+// import Link from "next/link";
+
+export default async function ProductPage() {
+  const response = await fetch("http://localhost:3001/products");
+  const products = await response.json();
   return (
-    <>
-      <h1>Product List</h1>
-      <h2>
-        <Link href="products/1">Product 1</Link>
-      </h2>
-      <h2>
-        <Link href="products/2">Product 2</Link>
-      </h2>
-      <h2>
-        <Link href="products/3" replace>
-          Product 3
-        </Link>
-      </h2>
-      <h2>
-        <Link href="products/4">Product 4</Link>
-      </h2>
-      <h2>
-        <Link href={`products/${productId}`}>Product {productId}</Link>
-      </h2>
-    </>
+    <ul className="space-y-4 p-4">
+      {products.map((product: Product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
+
+// export default function ProductList() {
+//   const productId = 100;
+//   return (
+//     <>
+//       <h1>Product List</h1>
+//       <h2>
+//         <Link href="products/1">Product 1</Link>
+//       </h2>
+//       <h2>
+//         <Link href="products/2">Product 2</Link>
+//       </h2>
+//       <h2>
+//         <Link href="products/3" replace>
+//           Product 3
+//         </Link>
+//       </h2>
+//       <h2>
+//         <Link href="products/4">Product 4</Link>
+//       </h2>
+//       <h2>
+//         <Link href={`products/${productId}`}>Product {productId}</Link>
+//       </h2>
+//     </>
+//   );
+// }
